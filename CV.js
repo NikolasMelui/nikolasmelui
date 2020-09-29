c0738ce1da0cf5143474c7646108df1c235281be
nikolasmelui <slak@samaradom.ru>
2020-09-30 00:58:37 +0400

    Add CV as the js file

---
+++ CV.js
@@ -0,0 +1,74 @@
+const nikolasmelui = {
+  name: 'Nikolai Sladkii',
+  birth: 1991,
+  place: 'russia, samara',
+  role: 'full-stack web developer (backend based)',
+  experience: '7+ years, strong',
+  contacts: {
+    phone: '+79093442211',
+    email: 'nikolasmelui@gmail.com',
+    socials: '@nikolasmelui',
+  },
+  skills: {
+    techs: ['node.js', 'golang', 'php', 'java', 'react/vue'],
+    tools: ['postgresql', 'mongodb', 'redis', 'rabbitmq', 'nginx', 'git', etc],
+    ops: ['gl ci/cd', 'gh actions', 'docker', 'k8s', 'ansible', etc],
+  },
+  plans: {
+    techs: ['rust'],
+    tools: ['4+1 avm', 'plantuml'],
+  },
+  love: ['*nix', 'vim', 'clean code', 'architecture', 'open source'],
+  work: [
+    {
+      company: 'mechanica',
+      projects: [
+        {
+          short: 'backend application for the of bonuses, discounts and cashbacks system',
+          role: 'lead, backend',
+          tech: 'php',
+          tasks: [
+            'backend API development',
+            'implementation of methods and adapters for communication with the side CMS system',
+            '... and diving in a really (overly) complex infrastructure!',
+          ],
+        },
+        {
+          short: 'backend application for the online poster of esports events',
+          role: 'lead, backend',
+          tech: 'node.js, strapi',
+          tasks: [
+            'application architecture design',
+            'backend API development',
+            'implementation of an additional intermediate service for collecting data from third-party systems and regularly updating the main poster information',
+            'development the push-notification module',
+          ],
+        },
+        {
+          short: 'golang microservice for b2b electronics portal (collecting, caching, changes checking and data sending (to the side system))',
+          role: 'backend',
+          tech: 'golang, redis',
+          tasks: [
+            'application architecture design',
+            'backend API development',
+            'implementation of the collecting and sending (to the side system) data mechanism',
+            'development the caching and changes checking mechanism',
+          ],
+        },
+        {
+          short: 'development of full-stack web back-office application for the iVolga 2.0 youth forum',
+          role: 'lead, backend, frontend',
+          tech: 'node.js, react',
+          tasks: [
+            'application architecture design',
+            'backend API development',
+            'frontend UI development',
+            'implementation of "inventory accounting", "registration", "authorization", "accreditation" etc. modules'
+          ],
+        },
+        etc
+      ],
+    }
+  ],
+  pet: ['The information will appear soon'],
+};
