22a2bcea98f63b3b15053a8e0f5a06743792b99c
nikolasmelui <slak@samaradom.ru>
2021-04-21 08:53:07 +0300

    Update CV.js

--- CV.js
+++ CV.js
@@ -1,32 +1,49 @@
 const nikolasmelui = {
   name: 'Nikolai Sladkii',
   birth: 1991,
-  place: 'russia, samara',
+  place: 'Russia, Moscow',
   role: 'full-stack web developer (backend based)',
-  experience: '7+ years, strong',
+  experience: 'about 8 years, strong',
   contacts: {
     phone: '+79093442211',
     email: 'nikolasmelui@gmail.com',
     socials: '@nikolasmelui',
   },
   skills: {
-    techs: ['node.js', 'golang', 'php', 'java', 'react/vue'],
-    tools: ['postgresql', 'mongodb', 'redis', 'rabbitmq', 'nginx', 'git', etc],
-    ops: ['gl ci/cd', 'gh actions', 'docker', 'k8s', 'ansible', etc],
+    techs: ['javascript', 'node.js', 'golang', 'php', 'java', 'frontend (react/vue)'],
+    tools: ['postgresql', 'mongodb', 'redis', 'rabbitmq', 'nginx', 'git', ...etc],
+    ops: ['gl ci/cd', 'gh actions', 'docker', 'k8s', 'ansible', ...etc],
   },
   plans: {
-    techs: ['rust'],
-    tools: ['4+1 avm', 'plantuml'],
+    techs: ['elixir', 'rust'],
+    tools: ['4+1 avm'],
   },
   love: ['*nix', 'vim', 'clean code', 'architecture', 'open source'],
   work: [
     {
-      company: 'mechanica',
+      company: 'voximplant',
+      actual: true,
       projects: [
         {
-          short: 'backend application for the of bonuses, discounts and cashbacks system',
-          role: 'lead, backend',
-          tech: 'php, laravel',
+          short:
+            'VoxEngine - an application engine that runs your apps inside the VoxImplant cloud',
+          role: 'senior backend',
+          tech: ['javascript', 'node.js'],
+          tasks: [
+            'serverless cloud platform lib and API development',
+          ],
+        },
+      ],
+    },
+    {
+      company: 'mechanica',
+      actual: false,
+      projects: [
+        {
+          short:
+            'backend application for the of bonuses, discounts and cashback system',
+          role: 'lead backend',
+          tech: ['php', 'laravel'],
           tasks: [
             'backend API development',
             'implementation of methods and adapters for communication with the side CMS system',
@@ -34,9 +51,9 @@ const nikolasmelui = {
           ],
         },
         {
-          short: 'backend application for the online poster of esports events',
-          role: 'lead, backend',
-          tech: 'node.js, strapi',
+          short: 'backend application for the online poster of ESports events',
+          roles: 'lead backend',
+          tech: ['node.js', 'strapi'],
           tasks: [
             'application architecture design',
             'backend API development',
@@ -45,9 +62,10 @@ const nikolasmelui = {
           ],
         },
         {
-          short: 'golang microservice for b2b electronics portal (collecting, caching, changes checking and data sending (to the side system))',
-          role: 'backend',
-          tech: 'golang, redis',
+          short:
+            'golang microservice for b2b electronics portal (collecting, caching, changes checking and data sending (to the side system))',
+          role: 'senior backend',
+          tech: ['golang', 'redis'],
           tasks: [
             'application architecture design',
             'backend API development',
@@ -56,19 +74,20 @@ const nikolasmelui = {
           ],
         },
         {
-          short: 'full-stack web back-office application for the iVolga 2.0 youth forum',
-          role: 'lead, backend, frontend',
-          tech: 'node.js, loopback, react',
+          short:
+            'full-stack web back-office application for the iVolga 2.0 youth forum',
+          role: 'lead backend',
+          tech: ['node.js', 'loopback', 'react'],
           tasks: [
             'application architecture design',
             'backend API development',
             'frontend UI development',
-            'implementation of "inventory accounting", "registration", "authorization", "accreditation" etc. modules'
+            'implementation of "inventory accounting", "registration", "authorization", "accreditation" etc. modules',
           ],
         },
-        etc
+        ...etc,
       ],
-    }
+    },
   ],
   pet: ['The information will appear soon'],
 };
